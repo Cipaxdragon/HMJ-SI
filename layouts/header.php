cda0bf0d9a5b9f387e73c9ae5c08bb8740531e9c
Asep <asep@gmail.com>
2023-10-19 23:27:16 +0800

    tiadakan internal css

--- layouts/header.php
+++ layouts/header.php
@@ -4,12 +4,12 @@
       <meta charset="UTF-8">
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="stylesheet" href="assets\css\gaya.css">
       <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
-      <link rel="stylesheet" href="assets\css\bootstrap.min.css">
+      <link rel="stylesheet" href="assets\css\bootstrap.css">
       <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
       <script src="assets\js\bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
       <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
-      <link rel="stylesheet" href="./assets/css/gaya.css">
       <title>SISFO UINAM</title>
       <link rel="shortcut icon" href="./image/android-chrome-192x192.png" type="image/x-icon">
       <style>
@@ -66,4 +66,3 @@
          </div>
       </nav>
 
-      <script src=""></script>
--- pages/beranda.php
+++ pages/beranda.php
@@ -1,113 +1,7 @@
 <!-- kotak  hero image -->
 <div class="kotakAtas container-fluid">
    <style>
-      .kotakAtas {
-         background-image: url('assets/image/Bg_hero.png');
-         padding: 0px;
-         background-color: white;
-      }
-
-      .judul {
-         justify-content: center;
-         align-items: center;
-         color: blue;
-         display: flex;
-      }
-
-      .hero_title_1 {
-         margin: 0px;
-         color: #1E357D;
-         font-family: Montserrat;
-         font-size: 28.023px;
-         font-style: normal;
-         font-weight: 700;
-         line-height: 100%;
-         /* 28.023px */
-         letter-spacing: -1.962px;
-      }
-
-      .hero_title_2 {
-         margin: 0px;
-         color: #1E357D;
-         font-family: Montserrat;
-         font-size: 51.064px;
-         font-style: normal;
-         font-weight: 700;
-         line-height: 100%;
-         letter-spacing: -3.574px;
-      }
-
-      .hero_title_3 {
-         margin: 0px;
-         color: #1E357D;
-         font-family: Montserrat;
-         font-size: 28.023px;
-         font-style: normal;
-         font-weight: 700;
-         line-height: 130%;
-         letter-spacing: -1.962px;
-      }
-
-      .hero_1 {
-         width: 1060px;
-      }
-
-      /* responsive */
-      @media (max-width: 768px) {
-
-         /* Styles for devices with a maximum width of 768px (e.g., mobile devices) */
-         /* .judul {
-            margin-top: 30px;
-            display: flex;
-
-         }
-
-         .judul img {
-            align-items: center;
-            width: 360px;
-            opacity: 30%;
-         }
-
-         .judul .hero_title {
-            margin-top: -100px;
-            position: absolute;
-            text-align: center;
-         } */
-
-         .judul {
-            align-items: center;
-            margin-top: 20px;
-            display: block;
-         }
-
-         .judul .hero_title {
-            text-align: center;
-         }
-
-         .judul .gambar-hero {
-            width: 360px;
-            margin: auto;
-         }
-
-         .judul .gambar-hero img {
-            align-items: center;
-            margin-top: -30px;
-            width: 360px;
-         }
-
-         .judul .hero_title_1 {
-            font-size: 20px;
-         }
-
-         .judul .hero_title_2 {
-            font-size: 40px;
-         }
-
-         .judul .hero_title_3 {
-            font-size: 20px;
-         }
-
-      }
+    
    </style>
    <div class="judul">
       <div class="hero_title">
@@ -127,90 +21,9 @@
 </div>
 <!-- tentang (kenali kami lebih lanjut) -->
 <style>
-   .container {
-      flex-direction: row;
-   }
-
-   .tentang {
-      color: #1E357D;
-      flex-direction: column;
-      /* display: flex; */
-      /* text-align: left; */
-      /* align-items: center; */
-      margin: auto;
-      font-size: 20px;
-      width: 100%;
-
-   }
-
-   .tt {
-      /* font-size: 40px; */
-      font-family: "Montserrat";
-      font-weight: 600;
-      height: 23px;
-   }
-
-   .ls {
-      font-size: 16px;
-      font-family: "Montserrat";
-      font-weight: 500;
-      width: 80%;
-   }
-
-   .gambar-logo img {
-      width: 400px;
-      margin-right: -80px;
-   }
-
-   #knli {
-      background-image: url('assets\image\tm.png');
-      font-size: 14.11px;
-      font-family: "Montserrat";
-      font-weight: 600;
-      color: rgba(77, 77, 77, 1);
-      height: 17px;
-      background-size: cover;
-      background-position: center;
-      background-repeat: no-repeat;
-      width: 213px;
-      height: 33px;
-      transition: transform 0.3s;
-   }
-
-   #knli:hover {
-      border-color: #1E357D;
-      color: #1E357D;
-      transform: scale(1.1);
-      background-color: white;
-   }
-
-   @media (max-width: 768px) {
-      .tentang {
-         display: flex;
-         text-align: center;
-         flex-direction: column;
-      }
-
-      .tt {
-         font-weight: bold;
-         margin-top: 40px;
-         font-size: 28px;
-      }
-
-      .gambar-logo img {
-         order: 1;
-         width: 250px;
-         margin: auto;
-      }
-
-      .ls {
-         font-size: 16px;
-      }
-
-
-   }
+  
 </style>
-<div class="container d-flex">
+<div id="b-tentang" class="container d-flex">
    <div class="tentang d-flex gap-3">
       <p class="tt">Tentang</p>
       <p class="ls"> Himpunan Mahasiswa Jurusan Sistem Informasi adalah suatu wadah yang menampung mahasiswa Sistem Informasi yang mengatur jalannya roda organisasi yang dihimpun dalam anggaran dasar dan anggaran rumah tangga untuk menciptakan suasanan kehidupan organisasi yang kondusif dan dinamis</p>
@@ -224,56 +37,7 @@
 </div>
 <!-- =========================== visimisi ================================ -->
 <style>
-   .visi-misi {
-      display: flex;
-      margin-top: 0;
-      /* justify-content: space-between ; */
-   }
-
-   .visi-misi img {
-      width: 600px;
-   }
-
-   @media (max-width: 768px) {
-      .visi-misi {
-         margin-left: -195px;
-      }
-
-      .visi-misi img {
-         width: 300px;
-         height: 350px;
-         margin: auto;
-      }
-
-      .isi-visi-misi {
-         width: 300px;
-         margin-left: 225px;
-         margin-top: -30px;
-         position: absolute;
-         display: block;
-
-      }
-
-      .head-visi,
-      .head-misi {
-         font-size: 20px;
-         font-weight: bold;
-         text-align: center;
-      }
-
-      .visi {
-         font-size: 12px;
-         text-align: center;
-      }
-
-      .misi {
-         margin-left: 40px;
-         font-size: 12px;
-         text-align: center;
-      }
-
-
-   }
+  
 </style>
 <!-- <div id="visimisi" class="visi-misi d-flex">
    <div>
@@ -298,56 +62,9 @@
 </div> -->
 <!-- =========================== Highlight ================================ -->
 <style>
-   .hailaig {
-      color: #1E357D;
-      background-color: white;
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      flex-wrap: nowrap;
-      align-content: center;
-      justify-content: center;
-      height: 617px;
-      text-align: center;
-   }
-
-   .hailaig img {
-      box-shadow: 0px 14px 34px 0px rgba(0, 0, 0, 0.25);
-      border-radius: 30px;
-      width: 900px;
-   }
-
-   .carousel-inner {
-      border-radius: 30px;
-      width: 900px;
-   }
-
-   .susunan {
-      display: flex !important;
-      justify-content: center;
-      align-items: center;
-      flex-direction: row;
-   }
-
-   @media (max-width: 768px) {
-      .hailaig {
-         margin-top: -180px;
-      }
-
-      .carousel-inner {
-         border-radius: 30px;
-         width: 370px;
-         margin: auto;
-      }
-
-      .susunan img {
-         width: 350px;
-         margin-top: 30px;
-      }
-   }
 </style>
 
-<div class="container hailaig">
+<div class="hailaig container">
    <h2>Info Penting</h2>
    <!-- <div class="susunan d-flex">
       <img src="assets\image\Berita_Kajur.png" alt="gambar">
@@ -355,13 +72,13 @@
    <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
       <div class="carousel-inner">
          <div class="carousel-item active">
-            <img src="assets\image\Berita_Kajur.png" class="d-block w-100" alt="...">
+            <img src="assets\image\Berita_Kajur.png" class="" alt="...">
          </div>
          <div class="carousel-item">
-            <img src="assets\image\Berita_Kajur.png" class="d-block w-100" alt="...">
+            <img src="assets\image\Berita_Kajur.png" class="d-block " alt="...">
          </div>
          <div class="carousel-item">
-            <img src="assets\image\Berita_Kajur.png" class="d-block w-100" alt="...">
+            <img src="assets\image\Berita_Kajur.png" class="d-block " alt="...">
          </div>
       </div>
       <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
@@ -424,164 +141,7 @@
 
    <!-- css  card kegiatan-->
    <style>
-      /* * {
-         margin: 0;
-         padding: 0;
-         box-sizing: border-box;
-         font-family: 'Poppins', sans-serif;
-      } */
 
-      /* body {
-         min-height: 100vh;
-         display: flex;
-         align-items: center;
-         justify-content: center;
-         background-color: #EFEFEF;
-      } */
-
-      .slide-container {
-         max-width: 1120px;
-         width: 100%;
-         padding: 40px 0;
-         margin: auto;
-      }
-
-      .slide-content {
-         margin: 0 40px;
-         overflow: hidden;
-         border-radius: 25px;
-      }
-
-      .card {
-         border-radius: 25px;
-         background-color: #FFF;
-      }
-
-      .image-content,
-      .card-content {
-         display: flex;
-         flex-direction: column;
-         align-items: center;
-         padding: 10px 14px;
-      }
-
-      .image-content {
-         position: relative;
-         row-gap: 5px;
-         padding: 25px 0;
-      }
-
-      .overlay {
-         position: absolute;
-         left: 0;
-         top: 0;
-         height: 100%;
-         width: 100%;
-         background-color: #4070F4;
-         border-radius: 25px 25px 0 25px;
-      }
-
-      .overlay::before,
-      .overlay::after {
-         content: '';
-         position: absolute;
-         right: 0;
-         bottom: -40px;
-         height: 40px;
-         width: 40px;
-         background-color: #4070F4;
-      }
-
-      .overlay::after {
-         border-radius: 0 25px 0 0;
-         background-color: #FFF;
-      }
-
-      .card-image {
-         position: relative;
-         height: 150px;
-         width: 150px;
-         border-radius: 50%;
-         background: #FFF;
-         padding: 3px;
-      }
-
-      .card-image .card-img {
-         height: 100%;
-         width: 100%;
-         object-fit: cover;
-         border-radius: 50%;
-         border: 4px solid #4070F4;
-      }
-
-      .name {
-         font-size: 18px;
-         font-weight: 500;
-         color: #333;
-      }
-
-      .description {
-         font-size: 14px;
-         color: #707070;
-         text-align: center;
-      }
-
-      .button {
-         border: none;
-         font-size: 16px;
-         color: #FFF;
-         padding: 8px 16px;
-         background-color: #4070F4;
-         border-radius: 6px;
-         margin: 14px;
-         cursor: pointer;
-         transition: all 0.3s ease;
-      }
-
-      .button:hover {
-         background: #265DF2;
-      }
-
-      .swiper-navBtn {
-         color: #6E93f7;
-         transition: color 0.3s ease;
-      }
-
-      .swiper-navBtn:hover {
-         color: #4070F4;
-      }
-
-      .swiper-navBtn::before,
-      .swiper-navBtn::after {
-         font-size: 35px;
-      }
-
-      /* .swiper-button-next {
-         right: 0;
-      }
-
-      .swiper-button-prev {
-         left: 0;
-      } */
-
-      .swiper-pagination-bullet {
-         background-color: #6E93f7;
-         opacity: 1;
-      }
-
-      .swiper-pagination-bullet-active {
-         background-color: #4070F4;
-      }
-
-      @media screen and (max-width: 768px) {
-         .slide-content {
-            margin: 0 10px;
-         }
-
-         .swiper-navBtn {
-            display: none;
-         }
-      }
    </style>
    <!-- akhir css kegiatan  -->
    <div class="slide-container">
