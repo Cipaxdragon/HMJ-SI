2a39885f148a4d71423e5d97dd02f8db5a2db4ac
Asep <asep@gmail.com>
2023-10-19 22:50:31 +0800

    Menambahkan Libary boostrap

--- layouts/footer.php
+++ layouts/footer.php
@@ -37,7 +37,7 @@
     </div>
       <!-- AKHIR FOOTER  -->
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js"></script>
+<script src="assets\js\bootstrap.bundle.min.js"></script>
 <script src="https://kit.fontawesome.com/7cfefc34bd.js" crossorigin="anonymous"></script>
 </body>
 
--- layouts/header.php
+++ layouts/header.php
@@ -5,9 +5,9 @@
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
-      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css">
+      <link rel="stylesheet" href="assets\css\bootstrap.min.css">
       <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+      <script src="assets\js\bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
       <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
       <link rel="stylesheet" href="./assets/css/gaya.css">
       <title>SISFO UINAM</title>
@@ -25,7 +25,6 @@
 ::-webkit-scrollbar-thumb:hover {
   background : #D6D6D6;
 }
-
 
       </style>
    </head>
